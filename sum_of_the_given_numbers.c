0c2674af26fd054ee7134319198477509294640d
Akhil987000 <102216458+Akhil987000@users.noreply.github.com>
2022-11-04 11:39:40 +0530

    #include <iostream> using namespace std; int main(){     int a,i;     cin>>a;     for(i=0;i<a;i++){         int x,y;         cin>>x>>y;         int z;         z=x+y;         cout<<z<<endl;
        }
        return 0;
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,i;
+    cin>>a;
+    for(i=0;i<a;i++){
+        int x,y;
+        cin>>x>>y;
+        int z;
+        z=x+y;
+        cout<<z<<endl;
+    
+    }
+    return 0;
+}
