e7f24e90ea51623770b605c2805e21fd4264a87c
Akhil987000 <102216458+Akhil987000@users.noreply.github.com>
2022-11-04 14:27:01 +0530

    #include <iostream> #include <cmath> using namespace std; int main(){     int a;     cin>>a;     if(a>0){         printf("Positive Number");     }     else{         printf("Negative Number");     }     return 0; }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if(a>0){
+        printf("Positive Number");
+    }
+    else{
+        printf("Negative Number");
+    }
+    return 0;
+}
